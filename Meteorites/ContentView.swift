831d9ad0d0f3ca8987e2a1ac8b5a091a1ee65d26
Roman Ligocki <roman.ligocki@uawareco.com>
2021-04-16 23:00:11 +0200

    Initial Commit

---
+++ Meteorites/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meteorites
+//
+//  Created by Roman Ligocki on 16.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meteorites/MeteoritesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteoritesApp.swift
+//  Meteorites
+//
+//  Created by Roman Ligocki on 16.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct MeteoritesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
